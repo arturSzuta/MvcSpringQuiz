bc790d8627df68420f119f581f7d40bb3342e82f
arturSzuta <artszuta@hotmail.com>
2023-06-03 10:05:00 +0200

    Added AdminQuizService

--- src/main/java/pl/sda/pol137/mvcspringquiz/controller/QuizAdminController.java
+++ src/main/java/pl/sda/pol137/mvcspringquiz/controller/QuizAdminController.java
@@ -6,10 +6,16 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import pl.sda.pol137.mvcspringquiz.model.QuizViewModel;
+import pl.sda.pol137.mvcspringquiz.service.AdminQuizService;
 
 @Controller
 @RequestMapping("/admin/quiz")
 public class QuizAdminController {
+    private final AdminQuizService quizService;
+
+    public QuizAdminController(AdminQuizService quizService) {
+        this.quizService = quizService;
+    }
 
     @GetMapping("/create")
     public String createForm() {
