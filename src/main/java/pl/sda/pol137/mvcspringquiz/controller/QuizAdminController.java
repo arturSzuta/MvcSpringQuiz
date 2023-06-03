e5504527cda427f6a5fb869003bf9c97383246ea
arturSzuta <artszuta@hotmail.com>
2023-06-03 10:29:13 +0200

    Added QuizMapper.java

--- src/main/java/pl/sda/pol137/mvcspringquiz/controller/QuizAdminController.java
+++ src/main/java/pl/sda/pol137/mvcspringquiz/controller/QuizAdminController.java
@@ -2,9 +2,12 @@ package pl.sda.pol137.mvcspringquiz.controller;
 
 import jakarta.validation.Valid;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import pl.sda.pol137.mvcspringquiz.mapper.QuizMapper;
+import pl.sda.pol137.mvcspringquiz.model.Quiz;
 import pl.sda.pol137.mvcspringquiz.model.QuizViewModel;
 import pl.sda.pol137.mvcspringquiz.service.AdminQuizService;
 
@@ -22,9 +25,10 @@ public class QuizAdminController {
         return "/quiz/create";
     }
     @PostMapping("/create")
-    public String createQuiz(@Valid QuizViewModel quizViewModel) {
-
-       // quizService.saveQuiz();
+    public String createQuiz(@Valid QuizViewModel quizViewModel, Model model) {
+        final Quiz quiz = QuizMapper.mapToQuiz(quizViewModel);
+        quizService.saveQuiz(quiz);
+        model.addAttribute("quizzes", quizService.findAllQuizzes());
         return "/quiz/index";
     }
 }
